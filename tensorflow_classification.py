729c5cf318b2ddbe24b42208b73569732291deaa
SWang <swang@192.168.0.109>
2016-11-06 22:22:00 +0800

    CORRECTED CODE

--- tensorflow_classification.py
+++ tensorflow_classification.py
@@ -14,17 +14,18 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
 # Train
 training_iteration = 10000
 batch_size = 100
 display_step = 50
 with tf.Session() as sess:
-    sess.run(init)
+	sess.run(tf.initialize_all_variables())
 	for iter in range(training_iteration):
-  		batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-  		train_step.run({x: batch_xs, y_: batch_ys})
-  		if iter % display_step == 0:
-            print "Iteration: " + '%04d'% (iter + 1) + "Accuracy: " + "{:.9f}".format(sess.run(accuracy, feed_dict={X: train_X, Y: train_Y}))
-	print "Test Accuracy: " + "{:.9f}".format(sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+		batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+		train_step.run({x: batch_xs, y_: batch_ys})
+		if iter % display_step == 0:
+			print "Epoch:", '%04d' % (iter + 1), "accuracy =", "{:.9f}".format(
+				sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys}))
+	print "Test Accuracy: " + "{:.9f}".format(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
--- tensorflow_regression.py
+++ tensorflow_regression.py
@@ -2,35 +2,38 @@ import tensorflow as tf
 import numpy
 
 
+train_X = numpy.asarray([30.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0])
+train_Y = numpy.asarray([320.0, 360.0, 400.0, 455.0, 490.0, 546.0, 580.0])
+train_X /= 100.0
+train_Y /= 100.0
+
 X = tf.placeholder("float")
 y = tf.placeholder("float")
 W = tf.Variable(numpy.random.randn(), name="weight")
 b = tf.Variable(numpy.random.randn(),name="bias")
-
-
+n_samples = train_X.shape[0]
 y_pred = tf.add(tf.mul(X, W), b)
 
 
-cost = tf.reduce_sum(tf.pow(y_pred- y,2))/(2 * n_samples)
+cost = tf.reduce_sum(tf.pow((y_pred - y), 2))/(2 * n_samples)
 
 
-learning_rate =0.01
+learning_rate = 0.01
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-​
-train_X = numpy.asarray([30.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0])
-train_Y =numpy.asarray([320.0, 360.0, 400.0, 455.0, 490.0, 546.0, 580])
 
 
 training_epochs = 1000
 display_step = 50
+
 with tf.Session() as sess:
-    sess.run(init)
+    sess.run(tf.initialize_all_variables())
     for epoch in range(training_epochs):
-        for(x, y) in zip(train_X, train_Y):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
-        if epoch % display_step ==0:
-            print"Epoch:" + '%04d'% (epoch+1) + "cost=" + "{:.9f}".format(sess.run(cost, feed_dict={X: train_X, Y:train_Y})) + "W=" + sess.run(W),"b=", sess.run(b)
+        for (x_train, y_train) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={X: x_train, y: y_train})
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch + 1), "cost =", "{:.9f}".format(
+                sess.run(cost, feed_dict={X: train_X, y: train_Y})), \
+                "W =", sess.run(W), "b =", sess.run(b)
     print "Optimization Finished!"
-    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print "Training cost = " + training_cost + "W = " + sess.run(W) + "b=" + sess.run(b) + '\n'
+    training_cost = sess.run(cost, feed_dict={X: train_X, y: train_Y})
+    print "Training cost =", training_cost, "W =", sess.run(W), "b =", sess.run(b), '\n'
